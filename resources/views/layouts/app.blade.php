321c34181eaf15375f8306b37ae67b21d603dbfe
inu847 <adin72978@gmail.com>
2023-07-11 20:49:56 +0700

    update

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'SISTEM PENJADWALAN') }}</title>
+    <title>SISTEM PENJADWALAN</title>
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
